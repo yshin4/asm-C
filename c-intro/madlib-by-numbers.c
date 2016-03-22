889d1c089a6476f416ed8c12cefcf58879aa8957
yshin4 <yshin4@lion.lmu.edu>
2016-03-22 15:38:11 -0700

    adding c-intro folder with files c

---
+++ c-intro/madlib-by-numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* madlib_by_numbers(char* template, int word_count, char* words[]){
+	for(int i = 0; i< strlen(template); i+=1){
+		if(isdigits template[i])
+	}
+}
+
+int* 
+
+int main(){
+	return 0;
+}
